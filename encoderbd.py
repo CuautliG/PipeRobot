fb4bf8704003d1d09808a65cf9d1bbc0e0d6a8d4
Cuautli <cgarc074@uottawa.ca>
2020-05-20 19:57:32 +0100

    Upload new encoder files

---
+++ encoderbd.py
@@ -0,0 +1,35 @@
+import time
+import RPi.GPIO as GPIO
+
+#pins to use
+CA = 22
+CB = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CA,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(CB,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+counter = 0
+lastA = GPIO.input(CA)
+f = open("ebd.txt","w+")
+
+def my_callback():
+        global lastA
+        global counter
+        while True:
+                        stateA = GPIO.input(CA)
+                        if stateA != lastA:
+                                stateB = GPIO.input(CB)
+                                if stateB != stateA:
+                                        counter += 1
+                                else:
+                                        counter -=1
+                                t = time.localtime()
+                                ctime = time.strftime("%H:%M:%S",t)
+                                print (ctime)
+                                print (counter)
+                                f.write(ctime)
+                                f.write(" %d\n" %counter)
+                        lastA = stateA
+
+my_callback()
